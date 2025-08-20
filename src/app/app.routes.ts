5803bbede214edbe0c3f7923d365db158f023b07
bpage93 <bradpage93@gmail.com>
2025-08-20 10:02:55 -0600

    fixed route path for hero-list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { HeroList } from './hero-list/hero-list';
 
 export const routes: Routes = [{
-    path: '/hero-list', component: HeroList,
+    path: 'hero-list', component: HeroList,
 }];
